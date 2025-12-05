1d83a76bdf331d948021777341e41fc32301085e
damonSkappel <dskappel@cord.edu>
2025-12-04 21:53:10 -0600

    Fixed the form submission so that it would go into the nixon_norman_media database and added instructions of how to make the tables in the contactFormSubmissionSQL.txt

--- src/contactSubmit.php
+++ src/contactSubmit.php
@@ -5,28 +5,37 @@ $selectedJobType = $_POST['selectedJobType'];
 $jobDate = $_POST['jobDate'];
 $jobComments = $_POST['jobComments'];
 
+
 // DB connection
-$conn = new mysqli('localhost', 'root', '', 'contactForms');
+$conn = new mysqli('localhost', 'root', '', 'nixon_norman_media');
+
 
 if ($conn->connect_error) {
-    die('Connection failed: ' . $conn->connect_error);
+   die('Connection failed: ' . $conn->connect_error);
 }
+
 
 // SQL statement
 $stmt = $conn->prepare("
-    INSERT INTO contactFormSubmissions (email, jobDate, selectedJobType, jobComments)
-    VALUES (?, ?, ?, ?)
+   INSERT INTO contactFormSubmissions (email, jobDate, selectedJobType, jobComments)
+   VALUES (?, ?, ?, ?)
 ");
+
 
 $stmt->bind_param("ssss", $email, $jobDate, $selectedJobType, $jobComments);
 
+
 // submit
 if ($stmt->execute()) {
-    echo "Submission successful! Expect a response in roughly _ business days";
+   echo "Submission successful! Expect a response in roughly _ business days";
 } else {
-    echo "Error: " . $stmt->error;
+   echo "Error: " . $stmt->error;
 }
+
 
 $stmt->close();
 $conn->close();
 ?>
+
+
+
